fbc1118d28b2eaaa2c8d7fabe50d182577555cbb
afrancop05 <alejandrofranco@iescastelar.com>
2024-02-21 21:36:53 +0000

    Update: Posible estructuracion de contenido

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,35 +4,49 @@
 <div class="contenido">
     <div class="row justify-content-center">
         <div class="col-md-12">
-                <h1>Contenido de OpenMetaverse</h1>
-                <br>
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+            @if (session('status'))
+                <div class="alert alert-success" role="alert">
+                    {{ session('status') }}
+                </div>
+            @endif
 
-                    <table class="table table-bordered text-center">
-                        <thead>
-                            <tr class="table-secondary">
-                                <th>Propietario</th>
-                                <th>Tipo</th>
-                                <th>Archivo</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <td>Ale</td>
-                                <td>Mundo</td>
-                                <td>ejemplo1.xml</td>
-                            </tr>
-                            <tr>
-                                <td>Pepe</td>
-                                <td>Artefacto</td>
-                                <td>ejemplo2.xml</td>
-                            </tr>
-                        </tbody>
-                    </table>
+            @auth
+                @if ((auth()->check()) && auth()->user()->hasRole('admin'))
+                    <h1>Contenido de OpenMetaverse</h1>
+                @endif
+            @endauth
+            @auth
+                @if ((auth()->check()) && auth()->user()->hasRole('user'))
+                    <h1>Contenido OpenMetaverse de {{ Auth::user()->name }}</h1>
+                @endif
+            @endauth
+            <br>
+                <table class="table table-bordered text-center">
+                    <thead>
+                        <tr class="table-secondary">
+                            @auth
+                                @if ((auth()->check()) && auth()->user()->hasRole('admin'))
+                                    <th>Propietario</th>
+                                @endif
+                            @endauth
+                            <th>Tipo</th>
+                            <th>Archivo</th>
+                            <th>Visibilidad</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            @auth
+                                @if ((auth()->check()) && auth()->user()->hasRole('admin'))
+                                    <td>Ale</td>
+                                @endif
+                            @endauth
+                            <td>Mundo</td>
+                            <td>ejemplo1.xml</td>
+                            <td><input type="button" value="privado"></td>
+                        </tr>
+                    </tbody>
+                </table>
                 </div>
             </div>
         </div>
