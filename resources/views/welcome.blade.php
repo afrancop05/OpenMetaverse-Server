c19cd12f18d2714101d28f170bf66ee1787f020e
afrancop05 <alejandrofranco@iescastelar.com>
2024-02-21 19:01:46 +0000

    Add: Logo pantalla inicial

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -23,7 +23,7 @@
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    OpenMetaverse
+                    <img src="images/logo.png" alt="Logo" style="width: 30px"> Inicio
                 </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
@@ -69,7 +69,8 @@
         </nav>
 
         <main>
-            <div class="contenido">
+            <div class="inicio">
+                <h1>Bienvenido a OpenMetaverse</h1>
             </div>  
         </main>
     </div>
