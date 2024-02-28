b50968a4f736cd7cb9c1eededc8290b3056b744e
Daniel Píriz <dpirizc01@iescastelar.com>
2024-02-28 12:17:30 +0000

    Update: API WorldMaker

--- resources/views/WorldMaker/index.blade.php
+++ resources/views/WorldMaker/index.blade.php
@@ -3,30 +3,31 @@
 <world>
 	<head>
 		<meta charset="UTF-8"/>
-		<title>Mundo</title>
-		<meta name="description" content="Mundo autogenerado"/>
-		<meta name="author" content="Laravel"/>
+		<title>{{ $name }}</title>
+		<meta name="description" content="{{ $description }}"/>
+		<meta name="author" content="{{ $author }}"/>
 		<meta name="keywords" content="MVML"/>
 	</head>
 	<environment>
 		<sky type="generated"/>
 	</environment>
 	<scene>
-		<rigid position="0,3,-5" scale="{{ $size }}, {{ $size }}, {{ $size }}">
+		<rigid position="0,3,-5" scale="{{ $size }},{{ $size }},{{ $size }}">
 			<shape primitive="{{ $shape }}"></shape>
 			<collider shape="{{ $shape }}"/>
 		</rigid>
 		<static>
-			<shape primitive="plane" scale="1000,1000,1000"></shape>
+            <?php $half_size = $plane_size; ?>
+			<shape primitive="plane" scale="{{ $plane_size }},{{ $plane_size }},{{ $plane_size }}"></shape>
 			<collider shape="convex">
-				<point position="500,0,500"/>
-				<point position="-500,0,500"/>
-				<point position="-500,0,-500"/>
-				<point position="-500,0,500"/>
-				<point position="-500,0,-500"/>
-				<point position="500,0,-500"/>
+				<point position="{{ $half_size }},0,{{ $half_size }}"/>
+				<point position="{{ -$half_size }},0,{{ $half_size }}"/>
+				<point position="{{ -$half_size }},0{{ -$half_size }}"/>
+				<point position="{{ -$half_size }},0,{{ $half_size }}"/>
+				<point position="{{ -$half_size }},0,{{ -$half_size }}"/>
+				<point position="{{ $half_size }},0,{{ -$half_size }}"/>
 			</collider>
 		</static>
-		<light type="directional" shadow="soft" rotation="-45,0,0" position="0,2,2"></light>
-	</scene>
+		<light type="directional" shadow="{{ $light_shadow }}" rotation="{{ $light_direction }}" color="{{ $light_color }}"></light>
+    </scene>
 </world>
